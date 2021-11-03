266cc8376d385f5a26ea53e6de731e8357536e2d
up201806302 <up201806302@fe.up.pt>
2021-11-03 17:46:45 +0000

    Finalized first model. Too simple, predictions are all 100%

--- train_pipeline.py
+++ train_pipeline.py
@@ -28,7 +28,7 @@ loan_train = pd.read_csv("./data/loan_train.csv", sep=';')
 loan_test = pd.read_csv("./data/loan_test.csv", sep=';')
 districts = districts.rename(columns={'code ':'code'})
 
-account_dic = {'OWNER':1, 'DISPONENT':0}
+account_dic = {'OWNER':1, 'DISPONENT':2}
 frequency_dic = {'monthly issuance':2, 'weekly issuance':1, 'issuance after transaction':0}
 
 
@@ -41,5 +41,54 @@ data2 = data1.rename(columns={'date':'creation_date'})
 loan_train2 = loan_train.rename(columns={'date':'loan_date'})
 full_data = data2.merge(loan_train2, how="inner")
 
-clean_data = full_data.drop(columns=['disp_id', 'client_id', 'account_id', 'district_id', 'type'])
+clean_data = full_data.drop(columns=['disp_id', 'client_id', 'account_id', 'district_id', 'creation_date', 'loan_date'])
+joined_data = clean_data.drop_duplicates(subset=clean_data.columns[1:], keep="last") #props mano caio
+final_data = joined_data.drop(columns=['loan_id'])
+
+
+loan_test2 = loan_test.rename(columns={'date':'loan_date'})
+full_data_test = data2.merge(loan_test2, how="inner")
+
+clean_data_test = full_data_test.drop(columns=['disp_id', 'client_id', 'account_id', 'district_id', 'creation_date', 'loan_date'])
+joined_data_test = clean_data_test.drop_duplicates(subset=clean_data_test.columns[1:], keep="last")
+all_ids_test = joined_data_test['loan_id'].values
+final_data_test = joined_data_test.drop(columns=['loan_id', 'status'])
+
+
+
+train_split, test_split = train_test_split(final_data, test_size=0.25, stratify=final_data['status'])
+
+X_train = train_split.iloc[:, :-1].values
+y_train = train_split.iloc[:, -1].values
+X_test = test_split.iloc[:, :-1].values
+y_test = test_split.iloc[:, -1].values
+
+scaler = StandardScaler()
+scaler.fit(X_train)
+
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.fit_transform(X_test)
+
+
+dt_classifier = DecisionTreeClassifier(random_state=1)
+
+dt_grid_search = GridSearchCV(dt_classifier,
+                            param_grid={},
+                            scoring='roc_auc',
+                            cv=10)
+
+dt_grid_search.fit(X_train, y_train)
+predictions_train = dt_grid_search.predict(X_train)
+predictions_test = dt_grid_search.predict(X_test)
+
+predictions_competition = dt_grid_search.predict_proba(final_data_test)
+predictions_competition = pd.DataFrame(predictions_competition, columns=['Predicted', 'col2'])
+predictions_competition.drop('col2', axis=1, inplace=True)
+dataframetemp = pd.DataFrame(all_ids_test, columns=['Id'])
+dataframeids = pd.concat([dataframetemp, predictions_competition], axis=1)
+results = dataframeids.drop_duplicates(subset=['Id'], keep='first')
+
+
+results.to_csv('out.csv', index = False)
+
 
