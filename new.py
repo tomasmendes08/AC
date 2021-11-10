855418342d2ec7faf49eaf25ddf58fc6cb39a4a3
CoDyPhin <carlosdaniel.rl@gmail.com>
2021-11-10 12:22:53 +0000

    Finished processing and merging accounts and transactions

--- new.py
+++ new.py
@@ -8,8 +8,34 @@ Created on Tue Nov  9 20:15:02 2021
 import pandas as pd
 import numpy as np
 
+def generate_gender(month):
+    gender = month > 12
+    if gender: month -= 50
+    
+    return gender
+
+def convert_date(date):
+    year = date // 10000 + 1900
+    days = date % 100
+    month = date // 100 % 100
+    
+    gender = generate_gender(month)
+    
+    return (year, month, days, gender)  
+
+def process_date(df, col_list, col_name='date'):
+    count = 0
+    for i in col_list:
+        df[i] = df[col_name]
+        df[i] = df[i].apply(lambda x : convert_date(x)[count])
+        count+=1
+    df.drop(columns={col_name}, inplace=True)
+
 #loading clients data
 clients = pd.read_csv("./data/client.csv", sep=';')
+
+#processing birth date
+process_date(clients, ['byear', 'bmonth', 'bday', 'gender'],'birth_number')
 
 #loading disps data
 disps = pd.read_csv("./data/disp.csv", sep=';')
@@ -21,7 +47,7 @@ clients_disps = clients.merge(disps, on="client_id", how="inner")
 cards = pd.read_csv("./data/card_train.csv", sep=';')
 
 #rename type column to disp_type
-disps.rename(columns={'type ':'disp_type'}, inplace=True)
+disps.rename(columns={'type':'disp_type'}, inplace=True)
 
 #rename type column to card_type
 cards.rename(columns={'type':'card_type'}, inplace=True)
@@ -47,24 +73,32 @@ districts["no. of commited crimes '95 "] = np.where(districts["no. of commited c
 districts["no. of commited crimes '95 "] = pd.to_numeric(districts["no. of commited crimes '95 "])
 
 #merging clients, disps and cards with districts
-clients_disps_cards_districts = clients_disps_cards.merge(districts, on="district_id")
+clients_disps_cards_districts = clients_disps_cards.merge(districts, on="district_id", how='inner')
 
 
 #loading accounts data
 accounts = pd.read_csv("./data/account.csv", sep=';')
 
-def generate_gender(month):
-    gender = month > 12
-    if gender: month -= 50
-    
-    return gender
+#processing account creation date and frequency
 
-def convert_date(date):
-    year = date // 10000
-    days = date % 100
-    month = date // 100 % 100
-    
-    gender = generate_gender(month)
-    
-    return (year, month, days, gender)
-    
+accounts.rename(columns={'district_id':'acc_district_id'}, inplace=True)
+process_date(accounts,['cyear', 'cmonth', 'cday'])
+
+accounts = pd.concat([accounts.drop('frequency', axis=1), pd.get_dummies(accounts['frequency'])], axis=1)
+
+#merging clients, disps, cards and districts with accounts
+clients_disps_cards_districts_accounts = clients_disps_cards_districts.merge(accounts, on="account_id")
+
+#loading transaction data
+transactions = pd.read_csv('./data/trans_train.csv', sep=';')
+
+#processing transaction data
+transactions.drop(columns={'k_symbol','bank','account', 'operation'}, inplace=True)
+transactions['type'].replace({'withdrawal in cash':'withdrawal'}, inplace=True)
+transactions['type'] = transactions['type'].apply(lambda x : x == 'credit')
+transactions.rename(columns={'type':'trans_type'}, inplace=True)
+
+process_date(transactions, ['tyear', 'tmonth', 'tday'])
+
+#mergins clients, disps, cards, districts and accounts with transactions
+clients_disps_cards_districts_accounts_transactions = clients_disps_cards_districts_accounts.merge(transactions, on="account_id", how="inner")
